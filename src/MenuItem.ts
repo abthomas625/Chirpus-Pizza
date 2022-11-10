f655d335f2e8fe8f64770fbb6984490a39ccaba8
Abilena <abthomas625@gmail.com>
2022-11-09 18:45:27 -0600

    best wings/decent pizza

---
+++ src/MenuItem.ts
@@ -0,0 +1,5 @@
+export interface MenuItems{
+    name : string;
+    category : string;
+    price : number
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MenuItems } from 'src/MenuItem';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ChirpusPizza';
+
+  items : MenuItems[] = [{name:"Pepperoni Pizza", category:"pizza", price: 18}, 
+                        {name: "Veggie Pizza", category: "pizza", price: 16}, 
+                        {name: "Mango Habanero Wings", category: "wings", price: 13},
+                        {name: "Buffalo Wings", category: "wings", price: 12},
+                        {name: "Jalapeno Poppers", category: "appetizer", price: 6},
+                        {name: "Mozzarella Sticks", category: "appetizer", price: 4}
+];
 }
